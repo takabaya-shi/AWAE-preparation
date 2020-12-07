d83baca1b43ead3de963e20115e3d591b7a41571
takabayashi <56021519+takabaya-shi@users.noreply.github.com>
2020-12-07 18:53:01 +0900

    Update app.js

--- javascript/Nodejs/Buffer/app.js
+++ javascript/Nodejs/Buffer/app.js
@@ -18,22 +18,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+// パラメータの受け渡しをJSON形式にする
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static( path.join(__dirname, '/bower_components')));
 
+// session変数を使用する。req.session的な使いかた
 app.use(session({
+  // Cookie: session=eyJhZG1pbiI6Im5vIn0=; みたいな形でヘッダにセットされる
   name: 'session',
+  // cofig.session_keysを使ってセッションの値をHMACで署名する。なのでただCookieを変えるだけではだめ
   keys: config.session_keys,
+  // 多分HMAC署名を使うかどうか
   cookie: { secure: true }
   })
 );
 
 // set default session cookie to "admin=no"
 app.use(function(req, res, next) {
+  // リクエストのCookieヘッダがセットされてなければ
   if(req.session.admin === undefined) {
+    // {"admin":"no"} をbase64エンコードしｈたeyJhZG1pbiI6Im5vIn0=がCookieに付与
     req.session.admin = 'no';
   }
   next();
